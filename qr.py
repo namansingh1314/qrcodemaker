03393a835f06e69fe3ab5928224ef75560612f95
420jerkster <namansingh1460@gmail.com>
2023-08-14 07:26:08 +0530

    initial commit

---
+++ qr.py
@@ -0,0 +1,16 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+  
+  
+# replace the link with your desired link 
+s = "https://drive.google.com/drive/folders/1--GvIqUNBMZdGVogiqfocRTecddeuTBZ"
+  
+# for Generating QR code
+url = pyqrcode.create(s)
+  
+# Create and save the svg file naming "myqr.svg"
+url.svg("myqr.svg", scale = 8)
+  
+# Create and save the png file naming "myqr.png"
+url.png('myqr.png', scale = 6)
